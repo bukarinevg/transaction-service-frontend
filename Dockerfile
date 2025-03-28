1c3d885ed80f57c8b88b20f8f8f4b60d52b4cf9d
Evgenii Bukarin <bukarinevgeni@gmail.com>
2025-03-28 04:56:56 +0300

    update API URL configuration to use VITE_API_URL and clean up Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:21-alpine
 
 WORKDIR /app
 
+
 COPY package.json package-lock.json ./
 RUN npm install
 
--- src/api/axious.ts
+++ src/api/axious.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL as string;
+
+
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
